aca4350b64f22c13fd7141a72cd6ea719b7deb64
LupitaRiv <lupitariv1103@gmail.com>
2025-03-26 12:00:00 -0600

    Commit automático de bloq_servidor.py en 2025-03-26T12:00:00

--- bloq_servidor.py
+++ bloq_servidor.py
@@ -23,3 +23,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         except KeyboardInterrupt:
             print("\nServidor detenido.")
 # Commit automático del día 5
+# Commit automático del día 5
